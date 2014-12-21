857857a4f56a34cbcd8f0804368e0a5a3ca5460d
nitoyon <nitoyon@gmail.com>
2014-12-22 01:20:05 +0900

    Test implementation for zoomable web interface with D3.js

--- tokyo/asset/js/boot.js
+++ tokyo/asset/js/boot.js
@@ -1,32 +1,58 @@
-$(window).on("load", function() {
-  // target to Imperial palaces
-  window.scrollTo(2400, 1700);
-  //initialize();
-});
+(function() {
+window.onload = function() {
+  var initPos = [-1600, -1450], initZoom = 2 / 3;
 
-function initialize(){
-  $("svg").panzoom({
-    minScale: 0.3,
-    maxScale: 2,
-    increment: 0.2,
-    duration: 600,
-    transition: true,
-    easing: "ease-in-out",
-    cursor: "move",
-  });
+  var zoom = d3.behavior.zoom()
+      .scale(initZoom)
+      .translate(initPos)
+      .scaleExtent([8/27, 1.5])
+      .on("zoom", zoomed);
+  d3.select("body").call(zoom);
 
-  $(".button.plus").on("click", function(){
-    $("svg").panzoom("zoom", getScale() * 1.2);
+  d3.select("#zoom-in").on("click", function() { changeZoom(3/2); } );
+  d3.select("#zoom-out").on("click", function() { changeZoom(2/3); } );
 
-  });
+  var svg = d3.select("#viewport");
+  zoomed();
 
-  $(".button.minus").on("click", function(){
-    $("svg").panzoom("zoom", getScale() * 0.8);
-  });
+  function changeZoom(factor) {
+    var svg = d3.select("svg").node();
+    var center = [svg.offsetWidth / 2, svg.offsetHeight / 2];
 
+    // http://bl.ocks.org/mgold/c2cc7242c8f800c736c4
+    var scale = zoom.scale(),
+        extent = zoom.scaleExtent(),
+        translate = zoom.translate(),
+        x = translate[0], y = translate[1],
+        targetScale = scale * factor;
+
+    // If the factor is too much, scale it down to reach the extent exactly
+    var clampedTargetScale = Math.max(extent[0], Math.min(extent[1], targetScale));
+    if (clampedTargetScale != targetScale){
+        targetScale = clampedTargetScale;
+        factor = targetScale / scale;
+    }
+
+    // Center each vector, stretch, then put back
+    console.log(translate, center, factor);
+    x = (x - center[0]) * factor + center[0];
+    y = (y - center[1]) * factor + center[1];
+
+    // Transition to the new view over 350ms
+    d3.transition().duration(350).tween("zoom", function () {
+        var interpolateScale = d3.interpolate(scale, targetScale),
+            interpolateTrans = d3.interpolate(translate, [x,y]);
+        return function (t) {
+            zoom.scale(interpolateScale(t))
+                .translate(interpolateTrans(t));
+            zoomed();
+        };
+    });
+  }
+
+  function zoomed() {
+    //console.log(zoom.translate(), zoom.scale());
+    svg.attr("transform", "translate(" + zoom.translate() + ") scale(" + zoom.scale() + ")");
+  }
 }
-
-function getScale(){
-  var scale = $("svg").panzoom("getMatrix")[0];
-  return scale;
-}
+})();
